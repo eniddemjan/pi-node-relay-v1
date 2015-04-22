7186abc3a3fc898e8ac9e71bb27478acb35dacea
eniddemjan <ajm.prg4@gmail.com>
2015-04-22 18:09:21 +0200

    Update interface.js

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,7 +6,7 @@ function buttonClick(clicked_id){
       type: 'GET',
       url: '/send',
       async: false,
-      data: {state: 'on'},
+      data: {state: 'ona'},
       success: function( data ) {
         json_data = jQuery.parseJSON(data);
       }
@@ -18,10 +18,59 @@ function buttonClick(clicked_id){
       type: 'GET',
       url: '/send',
       async: false,
-      data: {state: 'off'},
+      data: {state: 'offa'},
       success: function( data ) {
         json_data = jQuery.parseJSON(data);
       }
     });  
-  }  
+  }
+  
+    if (clicked_id == "3"){
+    $.ajax({
+      type: 'GET',
+      url: '/send',
+      async: false,
+      data: {state: 'onb'},
+      success: function( data ) {
+        json_data = jQuery.parseJSON(data);
+      }
+    });  
+  } 
+
+  if (clicked_id == "4"){
+    $.ajax({
+      type: 'GET',
+      url: '/send',
+      async: false,
+      data: {state: 'offb'},
+      success: function( data ) {
+        json_data = jQuery.parseJSON(data);
+      }
+    });  
+  }
+  
+    if (clicked_id == "5"){
+    $.ajax({
+      type: 'GET',
+      url: '/send',
+      async: false,
+      data: {state: 'onc'},
+      success: function( data ) {
+        json_data = jQuery.parseJSON(data);
+      }
+    });  
+  } 
+
+  if (clicked_id == "6"){
+    $.ajax({
+      type: 'GET',
+      url: '/send',
+      async: false,
+      data: {state: 'offc'},
+      success: function( data ) {
+        json_data = jQuery.parseJSON(data);
+      }
+    });  
+  }
+  
 }
